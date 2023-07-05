b152362f6477e7d70c851f15f1014df524de4fb0
therufuser <therufuser@gmail.com>
2023-07-06 01:16:57 +0200

    zsh: Add a `git-kak` wrapper
    This adds new command `git-kak` and `git-kak-init`.
    
    The idea behind these is that you can easily start a `kakoune` instance
    for the current git-repository and automagically connect to it.

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,2 +1,4 @@
+alias git-kak-init='systemctl --user start git-kak@$(systemd-escape $(git rev-parse --show-toplevel))'
+alias git-kak='if ! kak -l | grep -q ^$(basename $(git rev-parse --show-toplevel)); then git-kak-init; fi; kak -c $(basename $(git rev-parse --show-toplevel))'
 alias ls="exa --icons"
 alias ncdu="ncdu --color dark"
