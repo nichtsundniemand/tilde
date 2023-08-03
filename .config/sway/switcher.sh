4101a91576e3952b671a5eb5b1f81ed68b49c39a
nichtsundniemand <rufus.schaefing@gmail.com>
2023-08-04 00:18:34 +0200

    sway: Remove old crust and fluff from config

--- .config/sway/switcher.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-swaymsg -t get_tree |
-	jq '.nodes[].nodes[].nodes[] | select(.nodes==[]),select(.nodes!=[]).nodes[] | (.id|tostring)+ " " + .name' |
-	sed -e "s/\"//g" |
-	wofi -d -s .config/sway/wofi.css |
-	awk '{print "swaymsg \"[con_id=" $1 "]\" focus"}' |
-	sh
-
