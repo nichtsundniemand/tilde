5f1b525eea09971ba8c5fc957e75aed8da4da53f
nichtsundniemand <rufus.schaefing@gmail.com>
2023-09-04 15:14:53 +0200

    home-manager, sway: Refactor workspace-keybindings
    Since these bindings have a very similar structure, I decided to refactor
    that section into some map-calls.
    
    This saves some lines - not sure, if it makes it more readable though..

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -286,26 +286,6 @@ in
         "${modifier}+Shift+Down" = "move down";
         "${modifier}+Shift+Up" = "move up";
         "${modifier}+Shift+Right" = "move right";
-        "${modifier}+1" = "workspace 1";
-        "${modifier}+2" = "workspace 2";
-        "${modifier}+3" = "workspace 3";
-        "${modifier}+4" = "workspace 4";
-        "${modifier}+5" = "workspace 5";
-        "${modifier}+6" = "workspace 6";
-        "${modifier}+7" = "workspace 7";
-        "${modifier}+8" = "workspace 8";
-        "${modifier}+9" = "workspace 9";
-        "${modifier}+0" = "workspace 10";
-        "${modifier}+Shift+1" = "move container to workspace 1";
-        "${modifier}+Shift+2" = "move container to workspace 2";
-        "${modifier}+Shift+3" = "move container to workspace 3";
-        "${modifier}+Shift+4" = "move container to workspace 4";
-        "${modifier}+Shift+5" = "move container to workspace 5";
-        "${modifier}+Shift+6" = "move container to workspace 6";
-        "${modifier}+Shift+7" = "move container to workspace 7";
-        "${modifier}+Shift+8" = "move container to workspace 8";
-        "${modifier}+Shift+9" = "move container to workspace 9";
-        "${modifier}+Shift+0" = "move container to workspace 10";
         "${modifier}+b" = "splith";
         "${modifier}+v" = "splitv";
         "${modifier}+s" = "layout stacking";
@@ -324,7 +304,10 @@ in
         "XF86AudioMicMute" = "exec ${pkgs.pulseaudio}/bin/pactl set-source-mute @DEFAULT_SOURCE@ toggle";
         "XF86MonBrightnessDown" = "exec ${pkgs.brightnessctl}/bin/brightnessctl set 5%-";
         "XF86MonBrightnessUp" = "exec ${pkgs.brightnessctl}/bin/brightnessctl set +5%";
-      };
+      } //
+        (let mod = (n: d: n - builtins.mul d (builtins.div n d)); spaces = (builtins.genList (x: x + 1) 10); in
+          builtins.listToAttrs (builtins.map (space: {name="${modifier}+${toString (mod space 10)}"; value="workspace ${toString space}";}) spaces) //
+          builtins.listToAttrs (builtins.map (space: {name="${modifier}+Shift+${toString (mod space 10)}"; value="move container to workspace ${toString space}";}) spaces));
       modes = {
         resize = {
           "Left" = "resize shrink width 10px";
