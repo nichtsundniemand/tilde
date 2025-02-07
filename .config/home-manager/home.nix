3474c0d987da51a8a74edf1f21da686d639bfadd
nichtsundniemand <rufus.schaefing@gmail.com>
2025-02-07 01:00:44 +0100

    home-manager: Remove unnecessary doc-comment

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -99,17 +99,6 @@ in
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-
     # Make ALSA-clients play nice with pipewire
     ".config/alsa/asoundrc".text = ''
         ctl_type.pipewire {
